47063bdded655542e95c4caace9fa195185681b6
sngkeshav <72210379+sngkeshav@users.noreply.github.com>
2023-10-17 01:11:23 +0530

    repo setup

---
+++ src/test/java/org/finance/invoices/InvoicesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.finance.invoices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
