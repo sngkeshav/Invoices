1aefb62c0f4e857560df6eb885df565bb43debd3
keshavsingh0407 <keshav.singh@cogoport.com>
2024-05-02 15:58:30 +0530

    changes

--- src/main/java/org/finance/api/Application.java
+++ src/main/java/org/finance/api/Application.java
@@ -3,12 +3,24 @@ package org.finance.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 //@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
 //		SpringApplication.run(Application.class, args);
-        System.out.println("Application Started");
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int a = 0;
+        int b = 1;
+        int count = 2;
+        while (count <= n) {
+            b = b + a;
+            a = b - a;
+            count++;
+        }
+        System.out.println(b);
     }
 
 }
